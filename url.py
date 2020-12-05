0921d1c27b799665946b9241b74572e247be22fa
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2020-12-05 17:40:06 +0800

    update url.py: copy output to clipboard

--- url.py
+++ url.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-prefix = "https://raw.githubusercontent.com/CyrusYip/blog-static/main/"
+import pyperclip
+github = "https://raw.githubusercontent.com/CyrusYip/blog-static/main/"
+jsdeliver = "https://cdn.jsdelivr.net/gh/CyrusYip/blog-static/"
 while True:
-    path = input("Enter file path: ")
-    print(prefix+path)
+    path = input("Enter file path:")
+    print(f"\n![]({github+path})\n")
+    print(f"![]({jsdeliver+path})\n")
+    pyperclip.copy(f"![]({jsdeliver+path})")
