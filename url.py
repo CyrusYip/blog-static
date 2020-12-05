627322cd60716345d6867b69062101935453b17f
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2020-12-05 18:39:28 +0800

    update url.py

--- url.py
+++ url.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 import pyperclip
+import subprocess
+
 github = "https://raw.githubusercontent.com/CyrusYip/blog-static/main/"
 jsdeliver = "https://cdn.jsdelivr.net/gh/CyrusYip/blog-static/"
-while True:
-    path = input("Enter file path:")
+copy_path = "fzf | tr -d '\n' | xclip -selection c"
+
+def get_url():
+    subprocess.call(copy_path, shell=True)
+    path = pyperclip.paste()
     print(f"\n![]({github+path})\n")
     print(f"![]({jsdeliver+path})\n")
     pyperclip.copy(f"![]({jsdeliver+path})")
+
+while True:
+    if input("Press Enter to continue. Type something else to quit. ") == "":
+        get_url()
+    else:
+        break
